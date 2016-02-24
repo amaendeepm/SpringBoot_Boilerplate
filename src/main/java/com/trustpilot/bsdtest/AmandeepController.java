9a55c9d8f49ebf490d56cc6896bae782858c20a9
Amandeep Midha <amandeepm@gmx.com>
2016-02-24 17:05:48 +0100

    Lambdafy Controller

--- src/main/java/com/trustpilot/bsdtest/AmandeepController.java
+++ src/main/java/com/trustpilot/bsdtest/AmandeepController.java
@@ -3,52 +3,51 @@ package com.trustpilot.bsdtest;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
+
 import org.glassfish.jersey.client.ClientConfig;
 
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.trustpilot.bsdtest.MD5Service;
 
 @RestController
 public class AmandeepController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Please provide Morpheus' Ship Name in the format -> http://<<host>>:8080/ship/<<morpheusShipName>>";
+		return "Please provide Morpheus' Ship Name in the format -> http://<<host>>/ship/<<morpheusShipName>>";
 	}
 
 	@RequestMapping(value = "/ship/{name}")
-	public String getShip(@PathVariable String name) {
+	public String getShip(@PathVariable String name) throws NoSuchAlgorithmException {
 
 		System.out.println("Ship name input: " + name);
 
 		StringBuilder buf = new StringBuilder();
 		String answer = name; //Should be "Nebuchadnezzar"
-		MessageDigest m;
-		String answermd5 = "";
+		MD5Service md5 = s-> {
+				MessageDigest m;
+				m = MessageDigest.getInstance("MD5");
+				m.reset();
+				m.update(s.toLowerCase().getBytes());
+				byte[] digest = m.digest();
+				BigInteger bigInt = new BigInteger(1,digest);
+				String answermd5 = bigInt.toString(16);
+				while(answermd5.length() < 32)
+					answermd5 = "0"+answermd5;
+				return answermd5;
+		};
 
-		try {
-			m = MessageDigest.getInstance("MD5");
-			m.reset();
-			m.update(answer.toLowerCase().getBytes());
-			byte[] digest = m.digest();
-			BigInteger bigInt = new BigInteger(1,digest);
-			answermd5 = bigInt.toString(16);
-			while(answermd5.length() < 32)
-				answermd5 = "0"+answermd5;
-			System.out.println("MD5 of answer = " + answermd5);
-
-			} catch (NoSuchAlgorithmException e) {
-
-				e.printStackTrace();
-			}
+		String answermd5 = md5.md5Convert(answer);
 		
 		 ClientConfig config = new ClientConfig();
 		 Client client = ClientBuilder.newClient(config);
